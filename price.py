7c7cc4ffb6f0b615dcff4a261cbb99d0a208ded7
Evan Price <gevanprice@protonmail.com>
2023-12-15 16:18:39 -0600

    public cb candlestick data

--- price.py
+++ price.py
@@ -28,7 +28,8 @@ pd.set_option('display.max_columns', 50)
 
 # get all available coinbase crypto names tied to account
 def getNames():
-		
+	
+	wire.write("Available Cryptos:\n")
 	names = []
 	
 	try:
@@ -42,7 +43,32 @@ def getNames():
 		return 0		
 	
 	return names
+
+
+# get public candle data from coinbase
+def getCBcandles(nameInput):
 	
+	wire.write("\nCandle Data:\n\n")
+	
+	for name in nameInput:
+		try:
+			# 300 second granularity * 300 rows -> appx 1 day
+			raw = cbp.get_product_historic_rates(product_id = name + "-USD", granularity = 300)
+			
+			# chronological order, then send to pandas
+			raw.reverse()
+			df = pd.DataFrame(raw, columns = ["Date", "Open", "High", "Low", "Close", "Volume"]) 			
+			
+			# convert date from unix timestamp to readable format
+			df['Date'] = pd.to_datetime(df['Date'], unit='s')
+			
+			wire.write("\n" + name + "\n")
+			wire.write(str(df))
+			wire.write("\n")
+			
+		except:
+			wire.write("\n" + name + "Invalid\n")	
+
 
 
 
@@ -53,14 +79,15 @@ def getNames():
 
 start_time = time.time()	# track runtime
 
+cryptoNames = getNames()	# all cb wallets
 
-cryptoNames = getNames()
-
-# loop until error free 
+# loop until no errors
 while cryptoNames == 0:
 	cryptoNames = getNames()
 	
-print(str(cryptoNames))
+print(str(cryptoNames))		# console output
+
+getCBcandles(cryptoNames)	# open high low close volume
 
 
 end_time = time.time()
